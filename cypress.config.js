b97f870903501b6e87d32016c3f15149f3a1e740
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2022-12-28 11:51:32 +0100

    all changes for first test

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   viewportHeight:1080,
-  viewportWidth:1920
+  viewportWidth:1920,
   e2e: {
-    baseUrl: 'http://localhost:1234',
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-    excludeSpecPattern: "**/1-getting-started/*", "**/2-advanced-examples/*"
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
   }
 })
