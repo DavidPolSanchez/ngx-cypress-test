c4d8113eb51933c609fe710b8826ab2a17a3ff98
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2022-12-30 10:36:07 +0100

    fix spell problems

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -147,11 +147,51 @@ describe('Our First Suite', () => {
     cy.contains('Forms').click()
     cy.contains('Datepicker').click()
 
-    cy.contains('nb-card','Common Datepicker')
-      .find('input').then(input => {
+    cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
         cy.wrap(input).click()
         cy.get('nb-calendar-day-picker').contains('17').click()
-        cy.wrap(input).invoke('prop','value').should('contain','Dec 17, 2019')
+        cy.wrap(input).invoke('prop','value').should('contain', 'Dec 17, 2022')
       })
   })
+
+
+
+
+  it.only('radio button', () => {
+    cy.visit('/')
+    cy.contains("Forms").click()
+    cy.contains('Form Layouts').click()
+    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({force: true})
+          .should('be.checked')
+        cy.wrap(radioButtons)
+            .eq(1)
+            .check({force: true})
+        cy.wrap(radioButtons)
+            .first() 
+            .should( 'not.be.checked')
+    })
+  })  
+
+
+  it.only('assert event', () => {
+
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Toastr').click()
+
+    //cy.get('[type="checkbox"]').check({force: true})
+    cy.get('[type="checkbox"]').eq(0).click({force: true})
+    cy.get('[type="checkbox"]').eq(1).check({force: true})
+    
+  })
+
+
 })
+
+
+
+
+
